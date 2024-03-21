46f4544fa511df3b49419b4372489b30ea011ea8
nicokr01 <nico1.kreuzer1@gmail.com>
2024-03-21 19:42:07 +0100

    implement fuctionality of progress bar

--- angularTest/src/app/home/home.component.ts
+++ angularTest/src/app/home/home.component.ts
@@ -6,7 +6,6 @@ import { SidebarSmallLightComponent } from '../sidebar-small-light/sidebar-small
 import { FormsModule } from '@angular/forms';
 
 import { Auth } from '../Auth/auth';
-
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -19,6 +18,8 @@ import { Auth } from '../Auth/auth';
 export class HomeComponent extends Auth{
   protected input:string = "";
   protected inputActiveState:boolean = false;
+
+  protected test = 10;
 
   constructor(cookieService:CookieService){
     super(cookieService);
@@ -35,6 +36,10 @@ export class HomeComponent extends Auth{
     },200);
     /* //// Input span move animation check */
 
+
+    /* Responsive Design*/
+    
+    /* //// Responsive Design*/     
   }
 
 
@@ -72,4 +77,144 @@ export class HomeComponent extends Auth{
       }
     }
   }
+
+  // Cirlce
+  circle_one(end:any){
+    let circularProgress = document.getElementById("c-p-1");
+    let progressValue_e = document.getElementById("p-v-1");
+    var progressValue:string = "";
+    if(progressValue_e?.textContent?.toString != undefined){
+      progressValue = progressValue_e?.textContent;
+    }
+    
+    let start = parseInt(progressValue); 
+    if(end != parseInt(progressValue)){
+      let progressStartValue = start,   
+    progressEndValue = end,    
+    speed = 50;
+    
+    var betweenValue;
+    var rotation;
+    if(end > start){
+        rotation = "-";
+        betweenValue = end - start;
+    }
+    else{
+      rotation = "+";
+    }
+  
+  let progress = setInterval(() => {
+    progressStartValue++;
+  
+    if(progressValue_e){
+      progressValue_e.textContent = `${progressStartValue}%`;
+    }
+  
+    if(circularProgress){
+      circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #2B2F33 0deg)`;
+    }
+  
+    if(progressStartValue == progressEndValue){
+        clearInterval(progress);
+    }    
+  }, speed);
+    }
+}
+
+
+circle_two(end:any){
+  let circularProgress = document.getElementById("c-p-2");
+  let progressValue_e = document.getElementById("p-v-2");
+    var progressValue:string = "";
+    if(progressValue_e?.textContent?.toString != undefined){
+      progressValue = progressValue_e?.textContent;
+    }
+    
+    let start = parseInt(progressValue); 
+
+if(end != start){
+  let progressStartValue = start;    
+  var progressEndValue = end,    
+  speed = 5;
+
+  if(end < progressStartValue){
+  progressStartValue = 0;
+  }
+
+  let progress = setInterval(() => {
+  progressStartValue++;
+
+  if(progressValue_e){
+    progressValue_e.textContent = `${progressStartValue}%`
+  }
+  if(circularProgress){
+    circularProgress.style.background = `conic-gradient(green ${progressStartValue * 3.6}deg, #2B2F33 0deg)`
+  }
+    
+  if(progressStartValue == progressEndValue){
+      clearInterval(progress);
+  }    
+  }, speed);
+}
+
+}
+
+
+circle_three(end:any){
+  let circularProgress = document.getElementById("c-p-3");
+  let progressValue_e = document.getElementById("p-v-3");
+    var progressValue:string = "";
+      if(progressValue_e?.textContent?.toString != undefined){
+        progressValue = progressValue_e?.textContent;
+      }
+      
+      let start = parseInt(progressValue); 
+      if(end != start){
+      let progressStartValue = start;    
+      var progressEndValue = end,    
+      speed = 5;
+
+      if(end < progressStartValue){
+        progressStartValue = 0;
+      }
+
+      let progress = setInterval(() => {
+      progressStartValue++;
+
+      if(progressValue_e){
+        progressValue_e.textContent = `${progressStartValue}%`
+      }
+      
+      if(circularProgress){
+        circularProgress.style.background = `conic-gradient(red ${progressStartValue * 3.6}deg, #2B2F33 0deg)`
+      }
+
+      if(progressStartValue == progressEndValue){
+          clearInterval(progress);
+      }    
+      }, speed); 
+  }
+}
+
+  // //// Circle
+
+
+
+  /*
+    functionality System Frontend -v1.2.1
+
+    including Animation handler
+  */
+
+    compare(){
+
+      this.test+=10;
+      if(this.test >= 80){
+        this.test = 10;
+      }
+
+      this.circle_three(this.test);
+    }
+
+    /* //// end of System Frontent -v this->version*/
 }
