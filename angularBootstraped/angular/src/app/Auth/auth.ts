ad3d6145cd2f3f68a3da2d1bd237818787c0458c
nicokr01 <133851997+nicokr01@users.noreply.github.com>
2024-05-29 15:57:19 +0200

    Update auth.ts

--- angularBootstraped/angular/src/app/Auth/auth.ts
+++ angularBootstraped/angular/src/app/Auth/auth.ts
@@ -16,6 +16,7 @@ export class Auth{
         }
         else{
           const url = 'https://api.webolary.com/?checkCookie=&value='+this.cookieService.get("username");
+          console.log(url);
           await fetch(url)
           .then(response => response.json())
           .then(data => {
